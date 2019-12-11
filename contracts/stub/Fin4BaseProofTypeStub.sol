fc4a551f3599ffeeccdd63a42e4df1253220d0d3
Benjamin Degenhart <b.degenhart.study@gmail.com>
2019-12-11 12:47:53 +0100

    Delete contracts/

--- contracts/stub/Fin4BaseProofTypeStub.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity ^0.5.0;
-
-contract Fin4BaseProofTypeStub {
-
-  function getName() public view returns(string memory);
-
-}
--- contracts/stub/SensorSignalSubscriber.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity ^0.5.0;
-
-contract SensorSignalSubscriber { // used as interface TODO naming-scheme? stub vs. interface...
-
-    function sensorSignalReceived(string memory sensorID, uint timestamp, string memory data) public;
-
-}
