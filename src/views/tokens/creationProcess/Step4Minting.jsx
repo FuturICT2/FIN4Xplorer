dfd6f11e90e848c2782e1b5defdc53587de65e76
Benjamin Degenhart <b.degenhart.study@gmail.com>
2020-01-10 09:50:04 +0100

    Use state in step 4

--- src/views/tokens/creationProcess/Step4Minting.jsx
+++ src/views/tokens/creationProcess/Step4Minting.jsx
@@ -15,7 +15,7 @@ function StepMinting(props) {
 	const { t } = useTranslation();
 
 	const [draftId, setDraftId] = useState(null);
-	const value = useRef({});
+	const [value, setValue] = useState(PROPERTY_DEFAULT);
 
 	const getValue = (draft, prop) => {
 		return draft.value.hasOwnProperty(prop) ? draft.value[prop] : PROPERTY_DEFAULT[prop];
@@ -29,11 +29,11 @@ function StepMinting(props) {
 
 		let fixed = getValue(draft, 'fixedQuantity');
 		let userDef = getValue(draft, 'userDefinedQuantityFactor'); // TODO rename to tokenCreatorDefined...?
-		value.current = {
+		setValue({
 			fixedQuantity: fixed,
 			userDefinedQuantityFactor: userDef,
 			unit: getValue(draft, 'unit')
-		};
+		});
 
 		if (!(fixed === 0 || userDef === 0)) {
 			alert('Both fixedQuantity and userDefinedQuantityFactor are set. One of them must be zero.');
@@ -50,28 +50,20 @@ function StepMinting(props) {
 		setDraftId(draft.id);
 	});
 
+	const updateVal = (key, val) => {
+		setValue({
+			...value,
+			[key]: val
+		});
+	};
+
 	const submit = () => {
-		if (choice === 'fixedQuantity') {
-			value.current.userDefinedQuantityFactor = 0;
-		}
-
-		if (choice === 'userDefinedQuantityFactor') {
-			value.current.fixedQuantity = 0;
-		}
-
-		if (choice === 'isMintableFalse') {
-			// TODO otherwise errors on the smart contract
-			// handle this differently? #ConceptualDecision
-			value.current.fixedQuantity = 1;
-			value.current.userDefinedQuantityFactor = 0;
-		}
-
 		props.dispatch({
 			type: 'UPDATE_TOKEN_CREATION_DRAFT_FIELDS',
 			draftId: draftId,
 			lastModified: moment().valueOf(),
 			nodeName: 'value',
-			node: value.current
+			node: value
 		});
 		props.handleNext();
 	};
@@ -80,74 +72,84 @@ function StepMinting(props) {
 
 	return (
 		<>
-			{draftId && (
-				<>
-					{choice === 'isMintableFalse' && (
-						<center style={{ fontFamily: 'arial', color: 'orange' }}>
-							You set your token to not be mintable in the design step. Approving claims won't mint a balance to the
-							claimer.
-						</center>
-					)}
-					<table>
-						<tbody>
-							<tr>
-								<td style={{ width: '50%' }}>
-									<FormControlLabel
-										disabled={choice === 'isMintableFalse'}
-										checked={choice === 'fixedQuantity'}
-										control={<Radio />}
-										label="Fixed amount"
-										onChange={e => setChoice('fixedQuantity')}
-									/>
-								</td>
-								<td>
-									<TextField
-										disabled={choice !== 'fixedQuantity'}
-										type="number"
-										label="per claim"
-										defaultValue={value.current.fixedQuantity}
-										onChange={e => (value.current.fixedQuantity = e.target.value)}
-									/>
-								</td>
-							</tr>
-							<tr>
-								<td>&nbsp;</td>
-								<td>&nbsp;</td>
-							</tr>
-							<tr>
-								<td>
-									<FormControlLabel
-										disabled={choice === 'isMintableFalse'}
-										checked={choice === 'userDefinedQuantityFactor'}
-										control={<Radio />}
-										label="Fixed factor"
-										onChange={e => setChoice('userDefinedQuantityFactor')}
-									/>
-								</td>
-								<td>
-									<TextField
-										disabled={choice !== 'userDefinedQuantityFactor'}
-										type="number"
-										label="multiplication factor"
-										defaultValue={value.current.userDefinedQuantityFactor}
-										onChange={e => (value.current.userDefinedQuantityFactor = e.target.value)}
-										style={{ paddingBottom: '10px' }}
-									/>
-									<TextField
-										disabled={choice !== 'userDefinedQuantityFactor'}
-										type="text"
-										label="unit"
-										defaultValue={value.current.unit}
-										onChange={e => (value.current.unit = e.target.value)}
-									/>
-								</td>
-							</tr>
-						</tbody>
-					</table>
-					<br />
-					<StepsBottomNav nav={props.nav} handleNext={submit} />
-				</>
+			{choice === 'isMintableFalse' && (
+				<center style={{ fontFamily: 'arial', color: 'orange' }}>
+					You set your token to not be mintable in the design step. Approving claims won't mint a balance to the
+					claimer.
+				</center>
 			)}
+			<table>
+				<tbody>
+					<tr>
+						<td style={{ width: '50%' }}>
+							<FormControlLabel
+								disabled={choice === 'isMintableFalse'}
+								checked={choice === 'fixedQuantity'}
+								control={<Radio />}
+								label="Fixed amount"
+								onChange={e => {
+									setChoice('fixedQuantity');
+									setValue({
+										...value,
+										fixedQuantity: 1,
+										userDefinedQuantityFactor: 0
+									});
+								}}
+							/>
+						</td>
+						<td>
+							<TextField
+								disabled={choice !== 'fixedQuantity'}
+								type="number"
+								label="per claim"
+								value={value.fixedQuantity}
+								onChange={e => updateVal('fixedQuantity', Number(e.target.value))}
+							/>
+						</td>
+					</tr>
+					<tr>
+						<td>&nbsp;</td>
+						<td>&nbsp;</td>
+					</tr>
+					<tr>
+						<td>
+							<FormControlLabel
+								disabled={choice === 'isMintableFalse'}
+								checked={choice === 'userDefinedQuantityFactor'}
+								control={<Radio />}
+								label="Fixed factor"
+								onChange={e => {
+									setChoice('userDefinedQuantityFactor');
+									setValue({
+										...value,
+										fixedQuantity: 0,
+										userDefinedQuantityFactor: 1
+									});
+								}}
+							/>
+						</td>
+						<td>
+							<TextField
+								disabled={choice !== 'userDefinedQuantityFactor'}
+								type="number"
+								label="multiplication factor"
+								value={value.userDefinedQuantityFactor}
+								onChange={e => updateVal('userDefinedQuantityFactor', Number(e.target.value))}
+								style={{ paddingBottom: '10px' }}
+							/>
+							<TextField
+								disabled={choice !== 'userDefinedQuantityFactor'}
+								type="text"
+								label="unit"
+								value={value.unit}
+								onChange={e => updateVal('unit', e.target.value)}
+							/>
+						</td>
+					</tr>
+				</tbody>
+			</table>
+			<br />
+			<StepsBottomNav nav={props.nav} handleNext={submit} />
 		</>
 	);
 }
