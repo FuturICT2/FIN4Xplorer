33e06ef7af8b972be8b28022f797c35c5e614cba
Benjamin Aaron Degenhart <b.degenhart.study@gmail.com>
2020-03-21 12:44:46 +0100

    Create a simpler alternative to ContractForm

---
+++ src/components/ContractFormSimple.jsx
@@ -0,0 +1,93 @@
+import React, { useState, useEffect } from 'react';
+import { drizzleConnect } from 'drizzle-react';
+import { useTranslation } from 'react-i18next';
+import PropTypes from 'prop-types';
+import { contractCall } from './Contractor';
+import { TextField } from '@material-ui/core';
+import Button from './Button';
+import AddIcon from '@material-ui/icons/Add';
+import update from 'react-addons-update';
+
+function ContractFormSimple(props, context) {
+	const { t } = useTranslation();
+
+	const [isReady, setIsReady] = useState(false);
+	const [data, setData] = useState([]);
+
+	useEffect(() => {
+		if (isReady) {
+			return;
+		}
+		let dataObj = [];
+		for (let i = 0; i < props.fields.length; i++) {
+			dataObj.push('');
+		}
+		setData(dataObj);
+		setIsReady(true);
+	});
+
+	const doContractCall = () => {
+		console.log(data);
+		contractCall(
+			context,
+			props,
+			props.store.getState().fin4Store.defaultAccount,
+			props.contractName,
+			props.contractMethod,
+			data,
+			props.pendingTxStr ? props.pendingTxStr : props.contractMethod + '()',
+			() => {
+				props.callbackTxSuccess && props.callbackTxSuccess();
+			}
+		);
+	};
+
+	const updateVal = (index, val) => {
+		setData(
+			update(data, {
+				[index]: { $set: val }
+			})
+		);
+	};
+
+	return (
+		<span style={{ fontFamily: 'arial' }}>
+			{isReady ? (
+				<>
+					{props.fields.map((field, index) => {
+						return (
+							<TextField
+								key={index}
+								type={field[1]}
+								label={field[0]}
+								value={data[index]}
+								onChange={e => updateVal(index, e.target.value)}
+								style={inputFieldStyle}
+							/>
+						);
+					})}
+					<Button icon={AddIcon} onClick={doContractCall} center="true">
+						Submit
+					</Button>
+				</>
+			) : (
+				'Loading...'
+			)}
+		</span>
+	);
+}
+
+const inputFieldStyle = {
+	width: '100%',
+	marginBottom: '15px'
+};
+
+ContractFormSimple.contextTypes = {
+	drizzle: PropTypes.object
+};
+
+const mapStateToProps = state => {
+	return {};
+};
+
+export default drizzleConnect(ContractFormSimple, mapStateToProps);
