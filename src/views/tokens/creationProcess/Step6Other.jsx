d6cf9ebe704b5690e6b36542218a1db05bc2f3ec
Benjamin Aaron Degenhart <b.degenhart.study@gmail.com>
2020-04-13 12:42:00 +0200

    Wire up new Step6 with constraints
    Code copied and modified from Step5Proving

--- src/views/tokens/creationProcess/Step6Other.jsx
+++ src/views/tokens/creationProcess/Step6Other.jsx
@@ -5,6 +5,12 @@ import moment from 'moment';
 import StepsBottomNav from './StepsBottomNav';
 import Dropdown from '../../../components/Dropdown';
 import Button from '../../../components/Button';
+import ArrowRightIcon from '@material-ui/icons/ArrowRight';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMinusCircle, faPlusSquare } from '@fortawesome/free-solid-svg-icons';
+import { TextField } from '@material-ui/core';
+import styled from 'styled-components';
+import { findProofTypeAddressByName } from '../../../components/utils';
 
 const PROPERTY_DEFAULT = {
 	constraints: {}
@@ -14,8 +20,10 @@ function StepOther(props) {
 	const { t } = useTranslation();
 
 	const [draftId, setDraftId] = useState(null);
-	const [other, setOther] = useState(null);
+	// const [other, setOther] = useState(null); // TODO utilize more sub-nodes when it becomes applicable
 	const [showDropdown, setShowDropdown] = useState(false);
+	const [constraintsAdded, setConstraintsAdded] = useState([]);
+	const constraints = useRef({});
 
 	useEffect(() => {
 		if (!props.draft || draftId) {
@@ -23,10 +31,13 @@ function StepOther(props) {
 		}
 		let draft = props.draft;
 
-		setOther({
-			constraints: draft.other.hasOwnProperty('constraints') ? draft.other.constraints : PROPERTY_DEFAULT.constraints
-		});
+		constraints.current = draft.other.hasOwnProperty('constraints')
+			? draft.other.constraints
+			: PROPERTY_DEFAULT.constraints;
 
+		setConstraintsAdded(
+			Object.keys(constraints.current).map(name => findProofTypeAddressByName(props.proofTypes, name))
+		);
 		setDraftId(draft.id);
 	});
 
@@ -36,19 +47,103 @@ function StepOther(props) {
 			draftId: draftId,
 			lastModified: moment().valueOf(),
 			nodeName: 'other',
-			node: other
+			node: {
+				constraints: constraints.current
+			}
 		});
 		props.handleNext();
 	};
 
+	const addConstraint = addr => {
+		let constraint = props.proofTypes[addr];
+		let name = constraint.label;
+
+		constraints.current[name] = {
+			parameters: {}
+		};
+
+		if (constraint.paramsEncoded) {
+			constraints.current[name].parameters = {};
+			constraint.paramsEncoded.split(',').map(paramStr => {
+				let paramName = paramStr.split(':')[1];
+				constraints.current[name].parameters[paramName] = null;
+			});
+		}
+
+		setConstraintsAdded(constraintsAdded.concat(addr));
+		setShowDropdown(false);
+	};
+
+	const removeConstraint = addr => {
+		setConstraintsAdded(constraintsAdded.filter(a => a !== addr));
+		delete constraints.current[props.proofTypes[addr].label];
+	};
+
 	return (
 		<>
+			{constraintsAdded.length > 0 && Object.keys(props.proofTypes).length > 0 && (
+				<div style={{ fontFamily: 'arial' }}>
+					{constraintsAdded.map((constraintAddress, index) => {
+						let constraint = props.proofTypes[constraintAddress];
+						let name = constraint.label;
+						return (
+							<div key={'constraint_' + index} style={{ paddingTop: '20px' }}>
+								<div
+									key={'constraintLabel_' + index}
+									title={constraint.description}
+									style={{ display: 'flex', alignItems: 'center' }}>
+									<ArrowRightIcon />
+									{name}
+									<FontAwesomeIcon
+										icon={faMinusCircle}
+										style={styles.removeIcon}
+										title="Remove constraint"
+										onClick={() => removeConstraint(constraintAddress)}
+									/>
+									{constraint.paramsEncoded.length > 0 && (
+										<FontAwesomeIcon
+											icon={faPlusSquare}
+											style={styles.plusIcon}
+											title="Since this constraint has parameters to set, it will require an extra transaction when creating the token"
+										/>
+									)}
+								</div>
+								{constraint.paramsEncoded &&
+									constraint.paramsEncoded.split(',').map((paramStr, paramIndex) => {
+										// e.g. uint:interval:days,uint:maxQuantity:quantity
+										let type = paramStr.split(':')[0];
+										let paramName = paramStr.split(':')[1];
+										let description = paramStr.split(':')[2];
+										let key = 'constraint_' + index + '_param_' + paramIndex;
+										return (
+											<span key={key}>
+												<TextField
+													type={type === 'uint' ? 'number' : 'text'}
+													label={
+														<>
+															<span>{paramName}</span>
+															{description && <small> ({description})</small>}{' '}
+														</>
+													}
+													defaultValue={constraints.current[name].parameters[paramName]}
+													onChange={e => (constraints.current[name].parameters[paramName] = e.target.value)}
+													style={styles.normalField}
+												/>
+											</span>
+										);
+									})}
+							</div>
+						);
+					})}
+				</div>
+			)}
+			{constraintsAdded.length > 0 && <Spacer />}
 			{showDropdown ? (
 				<Dropdown
-					onChange={e => {}}
+					onChange={e => addConstraint(e.value)}
 					options={Object.keys(props.proofTypes)
 						.filter(addr => props.proofTypes[addr].isConstraint)
-						//.filter(addr => !proofs.current[props.proofTypes[addr].label]) TODO
+						.filter(addr => !constraints.current[props.proofTypes[addr].label])
 						.map(addr => props.proofTypes[addr])}
 					label="Add token constraint"
 				/>
@@ -61,6 +156,29 @@ function StepOther(props) {
 		</>
 	);
 }
+
+const Spacer = styled.div`
+	height: 30px;
+`;
+
+const styles = {
+	removeIcon: {
+		color: 'lightsalmon',
+		width: '14px',
+		height: '14px',
+		paddingLeft: '7px'
+	},
+	plusIcon: {
+		color: 'lightgreen',
+		width: '16px',
+		height: '16px',
+		paddingLeft: '7px'
+	},
+	normalField: {
+		width: '80%',
+		margin: '8px 0 8px 25px'
+	}
+};
 
 const mapStateToProps = state => {
 	return {
