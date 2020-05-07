e586b8cb7e41a5d2c8e34a5d90acea0bc93c6f1e
Benjamin Aaron Degenhart <b.degenhart.study@gmail.com>
2020-05-07 18:10:18 +0200

    Strip off new underlyings from draft that weren't created on-chain

--- src/views/tokens/creationProcess/Step7Underlying.jsx
+++ src/views/tokens/creationProcess/Step7Underlying.jsx
@@ -26,7 +26,10 @@ function StepUnderlying(props) {
 			return;
 		}
 		let draft = props.draft;
-		setUnderlyings(draft.underlyings);
+		// that filter strips off new ones that were not created on-chain
+		// TODO the complete draft-way would be to store info for new ones
+		// and dispatch it to redux right here if its not there?
+		setUnderlyings(draft.underlyings.filter(pseudoId => props.allUnderlyings[pseudoId]));
 		setDraftId(draft.id);
 	});
 
