7a3ef8fce7d6c629158e9f4821033b2685278cf6
Benjamin Degenhart <b.degenhart.work@gmail.com>
2020-06-26 16:50:00 +0200

    Prepare listeners for all currently implemented contract events

--- src/middleware/ContractEventReceiver.js
+++ src/middleware/ContractEventReceiver.js
@@ -23,8 +23,38 @@ const subscribeToContractEvents = defaultAccount => {
 		socket.emit('register', defaultAccount);
 	});
 
+	socket.on('Fin4TokenCreated', values => {
+		console.log('Received Fin4TokenCreated contract event via notification server', values);
+	});
+	socket.on('NewMessage', values => {
+		console.log('Received NewMessage contract event via notification server', values);
+	});
+	socket.on('MessageMarkedAsRead', values => {
+		console.log('Received MessageMarkedAsRead contract event via notification server', values);
+	});
 	socket.on('ClaimSubmitted', values => {
-		console.log('Frontend received ClaimSubmitted event', values);
+		console.log('Received ClaimSubmitted contract event via notification server', values);
+	});
+	socket.on('ClaimApproved', values => {
+		console.log('Received ClaimApproved contract event via notification server', values);
+	});
+	socket.on('ClaimRejected', values => {
+		console.log('Received ClaimRejected contract event via notification server', values);
+	});
+	socket.on('VerifierPending', values => {
+		console.log('Received VerifierPending contract event via notification server', values);
+	});
+	socket.on('VerifierApproved', values => {
+		console.log('Received VerifierApproved contract event via notification server', values);
+	});
+	socket.on('VerifierRejected', values => {
+		console.log('Received VerifierRejected contract event via notification server', values);
+	});
+	socket.on('UpdatedTotalSupply', values => {
+		console.log('Received UpdatedTotalSupply contract event via notification server', values);
+	});
+	socket.on('SubmissionAdded', values => {
+		console.log('Received SubmissionAdded contract event via notification server', values);
 	});
 };
 
