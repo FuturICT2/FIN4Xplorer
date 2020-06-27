42387cb3ed9e8326ef9ce9591c26b114504a8c24
Benjamin Degenhart <b.degenhart.work@gmail.com>
2020-06-27 12:51:40 +0200

    Build socket listeners in a loop

--- src/middleware/NotificationServerClient.js
+++ src/middleware/NotificationServerClient.js
@@ -1,4 +1,5 @@
 import io from 'socket.io-client';
+import { contractEventList, handleContractEvent } from './ContractEventHandler';
 
 let serverConfig = null;
 try {
@@ -26,39 +27,10 @@ const subscribeToContractEventsViaNotificationServer = defaultAccount => {
 		);
 		socket.emit('register', defaultAccount);
 	});
-
-	socket.on('Fin4TokenCreated', values => {
-		console.log('Received Fin4TokenCreated contract event via notification server', values);
-	});
-	socket.on('NewMessage', values => {
-		console.log('Received NewMessage contract event via notification server', values);
-	});
-	socket.on('MessageMarkedAsRead', values => {
-		console.log('Received MessageMarkedAsRead contract event via notification server', values);
-	});
-	socket.on('ClaimSubmitted', values => {
-		console.log('Received ClaimSubmitted contract event via notification server', values);
-	});
-	socket.on('ClaimApproved', values => {
-		console.log('Received ClaimApproved contract event via notification server', values);
-	});
-	socket.on('ClaimRejected', values => {
-		console.log('Received ClaimRejected contract event via notification server', values);
-	});
-	socket.on('VerifierPending', values => {
-		console.log('Received VerifierPending contract event via notification server', values);
-	});
-	socket.on('VerifierApproved', values => {
-		console.log('Received VerifierApproved contract event via notification server', values);
-	});
-	socket.on('VerifierRejected', values => {
-		console.log('Received VerifierRejected contract event via notification server', values);
-	});
-	socket.on('UpdatedTotalSupply', values => {
-		console.log('Received UpdatedTotalSupply contract event via notification server', values);
-	});
-	socket.on('SubmissionAdded', values => {
-		console.log('Received SubmissionAdded contract event via notification server', values);
+	contractEventList.map(eventName => {
+		socket.on(eventName, values => {
+			handleContractEvent(eventName, values);
+		});
 	});
 };
 
