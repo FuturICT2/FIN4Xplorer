b4873a17bec7a17877d625f917d48408be304f39
Benjamin Degenhart <b.degenhart.work@gmail.com>
2020-07-07 17:15:32 +0200

    This works to connect to DAppNode's IPFS if in the VPN

--- src/config/ipfs.js
+++ src/config/ipfs.js
@@ -1,4 +1,5 @@
 const IPFS = require('ipfs-http-client');
 const ipfs = new IPFS({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' });
+// const ipfs = new IPFS('ipfs.dappnode'); // requires VPN connection to DAppNode
 
 export default ipfs;
